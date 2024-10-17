2cf5cb2fa5c892d4ab7444d63ff1454454394d66
Minttu Hurme <ammsalme@users.noreply.github.com>
2024-10-17 15:41:21 +0300

    Openshift ; Dependency & maintenance update (#35) (#36)
    * Openshift ; Dependency & maintenance update (#35)
    
    * Openshift version
    * Do not use Oracle Instantclient
    * Use GitHubActions
    
    * Update deps
    
    * Update GitHubActions (#37)
    
    * 2.0.0-alpha.7

--- src/app.js
+++ src/app.js
@@ -17,95 +17,115 @@
 import express from 'express';
 import oracledb from 'oracledb';
 import HttpStatus from 'http-status';
-import {Utils} from '@natlibfi/melinda-commons';
+import {createLogger, createExpressLogger} from '@natlibfi/melinda-backend-commons';
 import {URLSearchParams} from 'url';
 import createMiddleware from './middleware';
 
 export default async function ({
-	enableProxy, httpPort,
-	alephLibrary, alephXServiceUrl, indexingPriority,
-	oracleUsername, oraclePassword, oracleConnectString
+  enableProxy, httpPort,
+  alephLibrary, alephXServiceUrl, indexingPriority,
+  oracleUsername, oraclePassword, oracleConnectString
 }) {
-	const {createLogger, createExpressLogger} = Utils;
-	const logger = createLogger();
+  const logger = createLogger();
 
-	const pool = await initOracle();
-	const server = initExpress();
+  const pool = await initOracle();
+  const server = initExpress();
 
-	server.on('close', async () => {
-		await pool.close(0);
-	});
+  server.on('close', async () => {
+    logger.debug(`Server close, closing pool.`);
+    await pool.close(0);
+  });
 
-	return server;
+  return server;
 
-	async function initOracle() {
-		setOracleOptions();
+  async function initOracle() {
+    logger.debug(oracledb.thin ? 'Running in thin mode' : 'Running in thick mode');
+    setOracleOptions();
 
-		logger.log('debug', 'Establishing connection to database...');
+    logger.debug('Establishing connection to database...');
+    logger.debug(`Oracle connectstring: ${oracleConnectString}`);
 
-		const pool = await oracledb.createPool({
-			user: oracleUsername, password: oraclePassword,
-			connectString: oracleConnectString
-		});
+    const pool = await oracledb.createPool({
+      user: oracleUsername, password: oraclePassword,
+      connectString: oracleConnectString
+    });
 
-		logger.log('debug', 'Connected to database!');
+    testConnection(pool);
 
-		return pool;
+    logger.debug('Connected to database!');
+    //logger.debug(`Pool debug: `);
 
-		function setOracleOptions() {
-			oracledb.outFormat = oracledb.OBJECT;
-			oracledb.poolTimeout = 20;
-			oracledb.events = false;
-			oracledb.poolPingInterval = 10;
-		}
-	}
+    return pool;
 
-	function initExpress() {
-		const app = express();
+    function setOracleOptions() {
+      // eslint-disable-next-line functional/immutable-data
+      oracledb.outFormat = oracledb.OBJECT;
+      // eslint-disable-next-line functional/immutable-data
+      oracledb.poolTimeout = 20;
+      // eslint-disable-next-line functional/immutable-data
+      oracledb.events = false;
+      // eslint-disable-next-line functional/immutable-data
+      oracledb.poolPingInterval = 10;
+    }
+  }
 
-		if (enableProxy) {
-			app.enable('trust proxy', true);
-		}
+  function initExpress() {
+    const app = express();
 
-		app.use(createExpressLogger({
-			msg: formatMessage
-		}));
+    // eslint-disable-next-line functional/no-conditional-statements
+    if (enableProxy) {
+      app.enable('trust proxy', true);
+    }
 
-		app.use(createMiddleware({pool, alephLibrary, indexingPriority, alephXServiceUrl}));
+    app.use(createExpressLogger({
+      msg: formatMessage
+    }));
 
-		app.use(handleError);
+    app.use(createMiddleware({pool, alephLibrary, indexingPriority, alephXServiceUrl}));
 
-		return app.listen(httpPort, () => logger.log('info', 'Started Aleph X-proxy'));
+    app.use(handleError);
 
-		function formatMessage(req, res) {
-			const newUrl = format();
-			return `${req.ip} HTTP ${req.method} ${newUrl} - ${res.statusCode}} ${res.responseTime}ms`;
+    return app.listen(httpPort, () => logger.info('Started Aleph X-proxy'));
 
-			function format() {
-				const [path, query] = req.url.split(/\?/);
-				const params = new URLSearchParams(query);
+    function formatMessage(req, res) {
+      const newUrl = format();
+      return `${req.ip} HTTP ${req.method} ${newUrl} - ${res.statusCode} ${res.responseTime}ms`;
 
-				params.delete('staff_user');
-				params.delete('staff_pass');
+      function format() {
+        const [path, query] = req.url.split(/\?/u);
+        const params = new URLSearchParams(query);
 
-				return `${path}?${params.toString()}`;
-			}
-		}
+        params.delete('staff_user');
+        params.delete('staff_pass');
 
-		async function handleError(err, req, res, next) { // eslint-disable-line no-unused-vars
-			const {
-				INTERNAL_SERVER_ERROR,
-				REQUEST_TIMEOUT
-			} = HttpStatus;
+        return `${path}?${params.toString()}`;
+      }
+    }
 
-			// Certain Oracle errors don't matter if the request was closed by the client
-			if (err.message && err.message.startsWith('NJS-018:') && req.aborted) {
-				res.sendStatus(REQUEST_TIMEOUT);
-				return;
-			}
+    // eslint-disable-next-line require-await
+    async function handleError(err, req, res, next) { // eslint-disable-line no-unused-vars
+      const {
+        INTERNAL_SERVER_ERROR,
+        REQUEST_TIMEOUT
+      } = HttpStatus;
 
-			res.sendStatus(INTERNAL_SERVER_ERROR);
-			throw err;
-		}
-	}
+      // Certain Oracle errors don't matter if the request was closed by the client
+      if (err.message && err.message.startsWith('NJS-018:') && req.aborted) {
+        logger.debug(`Request was closed by client, let's not mind closed connection`);
+        res.sendStatus(REQUEST_TIMEOUT);
+        return;
+      }
+      logger.debug(`We got an error!`);
+      res.sendStatus(INTERNAL_SERVER_ERROR);
+      throw err;
+    }
+  }
+
+  async function testConnection(pool) {
+    const connection = await pool.getConnection();
+    //logger.debug(connection.isHealthy());
+    logger.debug(`We tested a connection:`);
+    await connection.close();
+  }
+
 }
--- src/index.js
+++ src/index.js
@@ -20,37 +20,43 @@ import * as params from './config';
 run();
 
 async function run() {
-	let server;
+  // eslint-disable-next-line functional/no-let
+  let server;
 
-	registerInterruptionHandlers();
-	server = await startApp(params);
+  registerInterruptionHandlers();
+  // eslint-disable-next-line prefer-const
+  server = await startApp(params);
 
-	function registerInterruptionHandlers() {
-		process.on('SIGTERM', handleSignal);
-		process.on('SIGINT', handleSignal);
+  function registerInterruptionHandlers() {
+    process.on('SIGTERM', handleSignal);
+    process.on('SIGINT', handleSignal);
 
-		process.on('uncaughtException', ({stack}) => {
-			handleTermination({code: 1, message: stack});
-		});
+    process.on('uncaughtException', ({stack}) => {
+      handleTermination({code: 1, message: stack});
+    });
 
-		process.on('unhandledRejection', ({stack}) => {
-			handleTermination({code: 1, message: stack});
-		});
+    process.on('unhandledRejection', ({stack}) => {
+      handleTermination({code: 1, message: stack});
+    });
 
-		function handleSignal(signal) {
-			handleTermination({code: 1, message: `Received ${signal}`});
-		}
+    function handleSignal(signal) {
+      handleTermination({code: 1, message: `Received ${signal}`});
+    }
 
-		function handleTermination({code = 0, message}) {
-			if (server) {
-				server.close();
-			}
+    function handleTermination({code = 0, message}) {
+      // eslint-disable-next-line functional/no-conditional-statements
+      if (server) {
+        server.close();
+      }
 
-			if (message) {
-				console.error(message);
-			}
+      // eslint-disable-next-line functional/no-conditional-statements
+      if (message) {
+        // eslint-disable-next-line no-console
+        console.error(message);
+      }
 
-			process.exit(code);
-		}
-	}
+      // eslint-disable-next-line no-process-exit
+      process.exit(code);
+    }
+  }
 }
